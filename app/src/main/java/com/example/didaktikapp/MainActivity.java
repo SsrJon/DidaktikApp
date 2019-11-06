1d3b2bf878711c1e9f914af79824407700e3a2ff
Chac092 <45790760+Chac092@users.noreply.github.com>
2019-11-06 11:45:29 +0100

    Base de datos

--- app/src/main/java/com/example/didaktikapp/MainActivity.java
+++ app/src/main/java/com/example/didaktikapp/MainActivity.java
@@ -7,15 +7,33 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap mapa;
+    MapView mapaView;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        mapaView = findViewById(R.id.mapView);
 
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapaView.onCreate(mapViewBundle);
+        mapaView.getMapAsync(this);
     }
+
 
 
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
@@ -33,8 +51,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapa.setMinZoomPreference(12);
+        LatLng ny = new LatLng(40.7143528, -74.0059731);
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(ny));
+    }
 }
 
 
