4ca27b50692ee4e39bab4105f0cf58be38f711fd
Chac092 <45790760+Chac092@users.noreply.github.com>
2019-11-06 14:07:14 +0100

    clase abstracta

--- app/src/main/java/com/example/didaktikapp/MainActivity.java
+++ app/src/main/java/com/example/didaktikapp/MainActivity.java
@@ -23,7 +23,7 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
-public abstract class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap mapa;
     MapView mapaView;
     private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
@@ -46,11 +46,9 @@ public abstract class MainActivity extends AppCompatActivity implements OnMapRea
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
-
             //La posición del mapa y el nombre del marcador
             LatLng position = new LatLng(43.257385, -2.933527);
             String markerText = "Goazen";
-
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 Log.i("DEBUG", "onMapReady");
@@ -100,7 +98,20 @@ public abstract class MainActivity extends AppCompatActivity implements OnMapRea
     }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //La posición del mapa y el nombre del marcador
+        LatLng position = new LatLng(43.257385, -2.933527);
+        String markerText = "Goazen";
+        Log.i("DEBUG", "onMapReady");
 
+        //Añade el marcador
+        Marker marker = googleMap.addMarker(new MarkerOptions().position(position).title(markerText));
+
+        //Zoom del mapa
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(position, 19);
+        googleMap.animateCamera(cameraUpdate);
+    }
 }
 
 
