1070e152660897cafefb9756432561c1a340e498
Chac092 <45790760+Chac092@users.noreply.github.com>
2019-11-14 13:14:15 +0100

    TODO OK

---
+++ app/src/main/java/com/example/didaktikapp/pantallaCarga.java
@@ -0,0 +1,14 @@
+package com.example.didaktikapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallaCarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_carga);
+    }
+}
