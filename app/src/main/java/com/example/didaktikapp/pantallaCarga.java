4127aaf34bbebd5494b605d19ed475cba0f998c9
Chac092 <45790760+Chac092@users.noreply.github.com>
2019-11-15 14:14:54 +0100

    PANTALLA DE CARGA FUNCIONAL

--- app/src/main/java/com/example/didaktikapp/pantallaCarga.java
+++ app/src/main/java/com/example/didaktikapp/pantallaCarga.java
@@ -2,13 +2,31 @@ package com.example.didaktikapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class pantallaCarga extends AppCompatActivity {
+    ImageView progreso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_carga);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // acciones que se ejecutan tras los milisegundos
+                Intent intent = new Intent(pantallaCarga.this,MainActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
     }
+
 }
