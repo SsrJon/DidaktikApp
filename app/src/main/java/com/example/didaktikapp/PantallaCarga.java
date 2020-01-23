6e2961963ca93991bc067862e45e086dbb384480
Chac092 <45790760+Chac092@users.noreply.github.com>
2020-01-23 08:59:59 +0100

    Base de datos funcionando a falta de actualizar

--- app/src/main/java/com/example/didaktikapp/PantallaCarga.java
+++ app/src/main/java/com/example/didaktikapp/PantallaCarga.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,8 +26,9 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-
-
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 public class PantallaCarga extends AppCompatActivity {
@@ -117,35 +120,63 @@ public class PantallaCarga extends AppCompatActivity {
                     });
         }
 
-        private void BorrarBDLocal(){
+        private void DescargarBDLocal(){
             dbHelper = new DBHelper(getApplicationContext());
             dbsqlite = dbHelper.getWritableDatabase();
-            dbsqlite.execSQL("DELETE FROM "+ DBHelper.entidadLugares.TABLE_NAME);
+            Cursor cursorcantidad = dbsqlite.query(DBHelper.entidadLugares.TABLE_NAME,null,null,null,null,null,null);
+            while (cursorcantidad.moveToNext()){
+                int Id = cursorcantidad.getInt(cursorcantidad.getColumnIndexOrThrow(DBHelper.entidadLugares._ID));
+                String nom = cursorcantidad.getString(cursorcantidad.getColumnIndexOrThrow(DBHelper.entidadLugares.COLUMN_NAME_NOMBRE));
+                double Latitud = cursorcantidad.getDouble(cursorcantidad.getColumnIndexOrThrow(DBHelper.entidadLugares.COLUMN_NAME_LATITUD));
+                double Longitud = cursorcantidad.getDouble(cursorcantidad.getColumnIndexOrThrow(DBHelper.entidadLugares.COLUMN_NAME_LONGITUD));
+                Lugar L = new Lugar(Id,nom,Latitud,Longitud);
+                lugarOffline.add(L);
+            }
         }
 
         private void ActualizarBD(){
-        for (int i = 0 ; i<lugarOnline.size();i++){
-            dbHelper = new DBHelper(getApplicationContext());
-            dbsqlite = dbHelper.getWritableDatabase();
-          int ID =  lugarOnline.get(i).getIdlugar();
-          //   Toast.makeText(getApplicationContext(), "HOLA "+ID, Toast.LENGTH_SHORT).show();
-          String Nombre = lugarOnline.get(i).getNombre();
-          double Latitud = lugarOnline.get(i).getLatitud();
-          double Longitud = lugarOnline.get(i).getLongitud();
-            final String Insert_Data="INSERT INTO "+DBHelper.entidadLugares.TABLE_NAME+" VALUES('"+ID+"','"+Nombre+"',"+Latitud+","+Longitud+")";
-            dbsqlite.execSQL(Insert_Data);
-           /*ContentValues values = new ContentValues();
-           values.put(DBHelper.entidadLugares._ID,ID);
-           values.put(DBHelper.entidadLugares.COLUMN_NAME_NOMBRE,Nombre);
-           values.put(DBHelper.entidadLugares.COLUMN_NAME_LATITUD, Latitud);
-           values.put(DBHelper.entidadLugares.COLUMN_NAME_LONGITUD, Longitud);
-           dbsqlite.insert(DBHelper.entidadLugares.TABLE_NAME, null, values);*/
-        }
+        boolean Encontrado = false;
+            HashMap<Integer, Integer> comparacion = new HashMap<>();
+            comparacion.put(lugarOffline.get(0).getIdlugar(), 1);
+            comparacion.put(lugarOffline.get(1).getIdlugar(), 1);
+            comparacion.put(lugarOffline.get(2).getIdlugar(), 1);
+            comparacion.put(lugarOffline.get(3).getIdlugar(), 1);
+            comparacion.put(lugarOffline.get(4).getIdlugar(), 1);
+            comparacion.put(lugarOffline.get(5).getIdlugar(), 1);
+
+            for(int i=0;lugarOnline.size()>i;i++){
+                Encontrado = false;
+                for(int k = 0;lugarOffline.size()>k&&!Encontrado;k++){
+                    if(lugarOnline.get(i).getIdlugar()==lugarOnline.get(k).getIdlugar()){
+                        lugarOffline.get(k).setNombre(lugarOnline.get(i).getNombre());
+                        lugarOffline.get(k).setLatitud(lugarOnline.get(i).getLatitud());
+                        lugarOffline.get(k).setLongitud(lugarOnline.get(i).getLongitud());
+                        comparacion.get(k);
+                        comparacion.put(lugarOffline.get(k).getIdlugar(),0);
+                        Encontrado = true;
+                    }
+                }
+                if (!Encontrado){
+                    lugarOffline.add(lugarOnline.get(i));
+                }
+            }
+
+
+            Iterator<Map.Entry<Integer, Integer>> it = comparacion.entrySet().iterator();
+
+            while (it.hasNext()) {
+                Map.Entry<Integer, Integer> e = it.next();
+                if(e.getValue()==1){
+                    lugarOffline.remove(e.getKey());
+                }
+
+            }
+            Toast.makeText(getApplicationContext(),"Lugaresctualizados"+lugarOffline.size(),Toast.LENGTH_LONG).show();
             Actualizada = true;
         }
         public void comprobacion(){
             if (descargada){
-                BorrarBDLocal();
+                DescargarBDLocal();
                 ActualizarBD();
             }else{
                 Handler handler = new Handler();
